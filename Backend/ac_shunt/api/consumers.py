1aefe6b739a72e51c7b4b90d574844243cf911a2
bbaker5150_navair <barry.t.baker2.civ@us.navy.mil>
2026-04-22 14:10:57 -0700

    Sync debugging more ...

--- Backend/ac_shunt/api/consumers.py
+++ Backend/ac_shunt/api/consumers.py
@@ -846,6 +846,10 @@ class CalibrationConsumer(AsyncWebsocketConsumer):
                 'is_complete': False,
                 'message': f'{reading_type_base} stage data saved.'
             }))
+
+            await self.broadcast(text_data=json.dumps({
+                'type': 'live_sync_requested'
+            }))
         
         return True
     
